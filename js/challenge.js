fe4580fa315bbaaa9e87979373a8ee35e9532656
Ziru Zou <jiyucoding@yahoo.com>
2023-01-17 18:10:04 -0500

    add event listener on Like btn create new li element inside ul

--- js/challenge.js
+++ js/challenge.js
@@ -1,9 +1,12 @@
 const h1 = document.querySelector("#counter")
 let start = parseInt(h1.textContent)
 
+let likeBtn = document.querySelector("#heart")
+
 let pauseBtn = document.querySelector("#pause")
 const minusBtn = document.querySelector("#minus")
 const plusBtn = document.querySelector("#plus")
+
 let pause = false
 
 
@@ -15,6 +18,18 @@ document.addEventListener("DOMContentLoaded", ()=> {
     handleMinus();
     handlePlus();
 })
+
+// add event listener on "Like" btn
+likeBtn.addEventListener("click", (event) => {
+    let likesList = document.querySelector(".likes")
+    let li = document.createElement("li")
+    console.log(start)
+    li.setAttribute("data-num",  start)
+    li.textContent = `${start} has been liked 1 time `
+
+    likesList .append(li)
+})
+
 
 
 // handle plus and minus btn
