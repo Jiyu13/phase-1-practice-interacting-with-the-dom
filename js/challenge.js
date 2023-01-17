6143beff060697e8c16300758ad4bcd744fc7c95
Ziru Zou <jiyucoding@yahoo.com>
2023-01-17 17:49:41 -0500

    create handle plus and minus function, add DOMContentLoaded event

--- js/challenge.js
+++ js/challenge.js
@@ -1,10 +1,45 @@
-let pause = false
 const h1 = document.querySelector("#counter")
-let start = 0
+let start = parseInt(h1.textContent)
+
+let pauseBtn = document.querySelector("#pause")
+const minusBtn = document.querySelector("#minus")
+const plusBtn = document.querySelector("#plus")
+let pause = false
 
 
-setInterval(function () {
-    start += 1;
-    h1.textContent = start
-}, 1000)
+// add DOMCOntentLoaded event
+document.addEventListener("DOMContentLoaded", ()=> {
+    if (!pause) {
+        startCount();
+    }
+    handleMinus();
+    handlePlus();
+})
 
+
+// handle plus and minus btn
+function handleMinus() {
+    minusBtn.addEventListener(("click"), () => {
+        start -= 1
+        h1.textContent = start
+        console.log(h1.textContent)
+    })
+}
+
+function handlePlus() {
+    plusBtn.addEventListener(("click"), () => {
+        start += 1
+        h1.textContent = start
+        console.log(h1.textContent)
+    })
+}
+
+
+function startCount() {
+    setInterval(function () {
+        start += 1;
+        h1.textContent = start
+        console.log(h1.textContent)
+    }, 1000)
+    console.log(h1.textContent)
+}
