288e45ec580a39ab6b07df67b15341013c782710
Ziru Zou <jiyucoding@yahoo.com>
2023-01-17 20:13:23 -0500

    pause counter when clicking on pause btn and disable other buttons

--- js/challenge.js
+++ js/challenge.js
@@ -1,35 +1,43 @@
 const h1 = document.querySelector("#counter")
 let start = parseInt(h1.textContent)
 
-let likeBtn = document.querySelector("#heart")
 
+let likesList = document.querySelector(".likes")
+let likeBtn = document.querySelector("#heart")
 let pauseBtn = document.querySelector("#pause")
 const minusBtn = document.querySelector("#minus")
 const plusBtn = document.querySelector("#plus")
+let submitBtn = document.querySelector("#submit")
 
-let pause = false
+let isPaused = false
 
 
-// add DOMCOntentLoaded event
-document.addEventListener("DOMContentLoaded", ()=> {
-    if (!pause) {
-        startCount();
-    }
-    handleMinus();
-    handlePlus();
+let check = startCount()
+handleMinus();
+handlePlus();
+
+
+// handle pause counter
+pauseBtn.addEventListener("click", () => {
+    isPaused = !isPaused;
+    clearInterval(check)
+    pauseBtn.textContent = "resume"
+    minusBtn.disabled = true
+    plusBtn.disabled = true
+    likeBtn.disabled = true
+    submitBtn.disabled = true
+
 })
 
+
 // add event listener on "Like" btn
-likeBtn.addEventListener("click", (event) => {
-    let likesList = document.querySelector(".likes")
+likeBtn.addEventListener("click", () => {
     let li = document.createElement("li")
     console.log(start)
     li.setAttribute("data-num",  start)
     li.textContent = `${start} has been liked 1 time `
-
     likesList .append(li)
 })
-
 
 
 // handle plus and minus btn
@@ -51,10 +59,11 @@ function handlePlus() {
 
 
 function startCount() {
-    setInterval(function () {
-        start += 1;
-        h1.textContent = start
-        console.log(h1.textContent)
-    }, 1000)
-    console.log(h1.textContent)
+    if (!isPaused) {
+        return setInterval(function () {
+            start += 1;
+            h1.textContent = start
+            console.log(h1.textContent)
+        }, 1000)
+    }
 }
